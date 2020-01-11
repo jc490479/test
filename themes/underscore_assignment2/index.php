d10120eb620babdb134698a9192677737b84331e
ZhuKunBo <kunbo.zhu@my.jcu.edu.au>
2020-01-12 03:41:36 +0800

    plugin

--- themes/underscore_assignment2/index.php
+++ themes/underscore_assignment2/index.php
@@ -9,51 +9,44 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package Underscore_assignment2
+ * @package Assignment2
  */
+
 
 get_header();
 ?>
+<div class="container">
+	<div class="main">
+    	<?php if(have_posts()) : ?> 
+        	<?php while(have_posts()): the_post(); ?>
+    	<article class="post">
+        	<h3>
+        	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        	</h3>
+        	<div class="meta">
+        	Created By <?php the_author(); ?> on<?php the_time('F j, Y g:i a'); ?>
+        	</div>
+			<?php if(has_post_thumbnail()) : ?>
+    			<div class="post-thumbnail">
+        			<?php the_post_thumbnail(); ?>
+    			</div>
+			<?php endif; ?>
+        	<?php the_content(); ?>
+    	</article>
+	<br>
+	<a class="button" href="<?php the_permalink(); ?>"> Read More </a> 
+	<?php endwhile; ?> 
+    	<?php else : ?>
+        	<?php echo wpautop('Sorry, No posts were found'); ?>
+    	<?php endif; ?> 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	</div>
+<div class="sidebar">
+    <?php if(is_active_sidebar('sidebar')) : ?>
+    <?php dynamic_sidebar('sidebar'); ?>
+    <?php endif; ?>
+</div>  
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+	<div class="clr"></div>
+</div>
+<?php get_footer();  
