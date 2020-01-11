53bf0359677f8f91c269523e7ffa8d48a6270a41
ZhuKunBo <kunbo.zhu@my.jcu.edu.au>
2020-01-12 02:18:28 +0800

    commit test

--- themes/twentynineteen/footer.php
+++ themes/twentynineteen/footer.php
@@ -25,7 +25,7 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentynineteen' ) ); ?>" class="imprint">
 				<?php
 				/* translators: %s: WordPress. */
-				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'CMS footer change2' );
+				printf( __( 'Proudly powered by %s.', 'twentynineteen' ), 'CMS footer change3' );
 				?>
 			</a>
 			<?php
