fb67c3e6369cecbef74facbd62e7ab12b3b17c33
ZhuKunBo <kunbo.zhu@my.jcu.edu.au>
2020-01-13 14:50:43 +0800

    footer

--- themes/a2/footer.php
+++ themes/a2/footer.php
@@ -24,7 +24,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'a2' ), 'Underscore Assignment2', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'a2' ), 'Teamwork', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
